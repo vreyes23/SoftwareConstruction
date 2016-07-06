10fff5073870c96d761b3083fe53e9fdd7fc7b5d
lethompson <lethompson1024@gmail.com>
2016-07-06 15:23:18 -0600

    Updated classes

--- Node.java
+++ Node.java
@@ -1,11 +1,12 @@
 package cfg;
 
-public abstract class Node {
+public abstract class Node<T> {
 	private StartNode nodeA; //(Knows node A)
 	private EndNode nodeB; //(Knows node B)
 	private  boolean isentry; //Start Node member
     private boolean isexit; //End Node member
     private int nodenumber; //Node Number
+    private T NodeType; //Generic Type: (StartNode,EndNode,JoinNode,SplitNode)
     
 	public abstract Node getNode(); //Know its type
 	//Knows the Node number
@@ -20,9 +21,10 @@ public abstract class Node {
 	public boolean isEntry(){
 		return (false);
 	}
-	public abstract boolean isSameNode(Node node); //Tests whether its the same Node
-	public abstract void setExit(EndNode nd); //Set End Node
-	public abstract void setEntry(StartNode sn); //Set Start Node
-	public abstract void getisExit(); //Return End Node
-	public abstract void getisEntry(); //Return Start Node
+	//public abstract <N> N isSameNode(N node); //Tests whether its the same Node
+	public abstract boolean isSameNode(T NodeType);
+	//public abstract void setExit(EndNode nd); //Set End Node
+	//public abstract void setEntry(StartNode sn); //Set Start Node
+	//public abstract void getisExit(); //Return End Node
+	//public abstract void getisEntry(); //Return Start Node
 }
--- StatementGenerator.java
+++ StatementGenerator.java
@@ -1,6 +1,10 @@
 package cfg;
 
 import java.util.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class StatementGenerator {
 
@@ -63,7 +67,36 @@ public class StatementGenerator {
 		}//end for
 	}//end moveOpenBrace() procedure
 	
-	
+	public void readFileInput(String path)
+	   {
+			
+			FileInputStream fstream = null;
+			
+			try{
+				//Read in the input file passed through the param: path
+				fstream = new FileInputStream(path);
+				//Print “File accepted”
+				System.out.println("File accepted");
+			}//end try
+			catch (IOException e){
+				//Throw an Input Output Exception
+				System.err.println("Unable opening file "+path+".\n"+e.getMessage());
+				//Print “please try again, file not accepted”
+				System.out.println("Please try again, File not accepted");
+				System.exit(1);
+			}//end catch
+			
+			Scanner s = new Scanner(fstream);
+			s.close();
+			try{
+				//Close the input file after reading in all the contents of the File
+				fstream.close();
+			}//end try
+			catch (IOException e){
+				//Throw an Input/Output Exception if a problem occurs 
+				System.err.println("Error closing file "+path+".\n"+e.getMessage());
+			}//end catch
+	  }//end readFileInput(param) procedure
 	
 	
 
