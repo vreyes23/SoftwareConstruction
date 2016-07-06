744b34331890e45eb9f1c2ea0f4dec0c89281aca
vreyes23 <vareyes@miners.utep.edu>
2016-07-06 15:32:35 -0600

    Updated Hierarchy
    Updated Hierarchy with the Classes

--- CFGCreator/src/cfg/JoinNode.java
+++ CFGCreator/src/cfg/JoinNode.java
@@ -1,15 +1,31 @@
 package cfg;
 
 public class JoinNode {
-	
-	//Knows node B is null
-	
-	//Knows content is "Join"
-	
-	//Knows type is "JoinNode"
 
-	public JoinNode() {
-		// TODO Auto-generated constructor stub
-	}
+	private JoinNode joinnode;
+
+	public JoinNode(JoinNode join) 
+	{
+	     //Set joinnode equal to  join variable;
+		this.joinnode = join;
+	}//end Constructor
+
+	public JoinNode getJoinNode() //(Knows content is "Join")
+	{
+	     //Return  of joinnode private variable;
+		  return (this.joinnode);
+	}//end getJoinNode() function
+	
+    public void setJoinNode(JoinNode jn)
+	{
+	       //Set joinnode equal to join variable;
+    	this.joinnode = jn;
+	}//end setJoinNode() procedure
+
+	public boolean isJoinNode(JoinNode joinobj)
+	{
+	 //     Return if joinnode equates to join.getJoinNode();
+		   return (this.joinnode == joinobj.getJoinNode());
+	}//end isJoinNode() function
 
 }
